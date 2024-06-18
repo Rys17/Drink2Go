5b5948410ece21b2f9a19cafae5ea3de55d2a010
Руслан <ryslann95@mail.ru>
2024-06-18 05:33:33 +0300

    Преимущества, слайдер, немного формы

---
+++ source/scripts/nav.js
@@ -0,0 +1,24 @@
+const navList = document.querySelector('.header__nav-list');
+const navBurger = document.querySelector('.user-menu__burger');
+
+
+const onNavBurgerClick = () => {
+  if (navList.classList.contains('header__nav-list--active')) {
+    navList.classList.remove('header__nav-list--active');
+    navBurger.classList.remove('user-menu__burger--active');
+  } else {
+    navList.classList.add('header__nav-list--active');
+    navBurger.classList.add('user-menu__burger--active');
+  }
+};
+
+const navBurgerInit = () => {
+  if (navBurger) {
+    navBurger.addEventListener('click', onNavBurgerClick);
+  }
+};
+
+
+export {
+  navBurgerInit
+};
